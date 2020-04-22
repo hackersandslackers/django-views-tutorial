b7f32e4ad24a6e803b2ff4a6c1cdff0f8aae927b
Todd Birchard <toddbirchard@gmail.com>
2020-04-22 00:57:28 -0400

    Updated settings.py.

--- django_views_tutorial/settings.py
+++ django_views_tutorial/settings.py
@@ -16,6 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, "static"),
