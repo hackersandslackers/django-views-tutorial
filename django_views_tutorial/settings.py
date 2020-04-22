1f26b8e78bd1a28919a8122dd3443bb50fd48ac4
Todd Birchard <toddbirchard@gmail.com>
2020-04-22 19:20:53 -0400

    Resolved static file settings.

--- django_views_tutorial/settings.py
+++ django_views_tutorial/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': os.environ.get('DATABASE_PORT'),
-        'OPTIONS': {'ssl': {'pem': '../creds/ca-certificate.crt'}}
+        'OPTIONS': {'ssl': {'pem': os.environ.get('DATABASE_CERTIFICATE')}}
     }
 }
 
