3b5034915b3b0085e0cf57a34009c33be1b9a494
Todd Birchard <toddbirchard@gmail.com>
2020-04-22 00:41:03 -0400

    Updated settings.py.

--- django_views_tutorial/static/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let disabledLinks = document.getElementsByClassName('disabled');
-
-    for (let i = 0, len = disabledLinks.length; i < len; i++) {
-        disabledLinks[i].addEventListener('click', function (e) {
-            e.preventDefault();
-        });
-    }
-});
